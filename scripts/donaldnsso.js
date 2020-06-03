ea50f1acc1ab46c33de17fdacf7c34d7ee19988d
SalomonD <salomonnssow10@gmail.com>
2020-06-03 16:34:42 +0200

    change 1

--- scripts/donaldnsso.js
+++ scripts/donaldnsso.js
@@ -1,9 +1,2 @@
-const FULL_NAME = 'Salomon Donald NSSO BIDJA',
-        HNG_ID = 'HNG-00906',
-        LANGUAGE = 'Javascript',
-        EMAIL = "salomonnssow10@gmail.com"
-
-let output = "Hello World, this is " + FULL_NAME +
-        " with HNGi7 ID " + HNG_ID + " and email " + EMAIL + " using " + LANGUAGE + " for stage 2 task"
-
-console.log(output)
+console.log("Hello World, this is Salomon Donald NSSO BIDJA"+
+" with HNGi7 ID HNG-00906 and email salomonnssow10@gmail.com using Javascript for stage 2 task")
--- scripts/donaldnsso2.py
+++ scripts/donaldnsso2.py
@@ -1,8 +1,1 @@
-FULL_NAME = 'Salomon Donald NSSO BIDJA',
-HNG_ID = 'HNG-00906',
-LANGUAGE = 'Javascript',
-EMAIL = 'salomonnssow10@gmail.com'
-
-# output = 'Hello World, this is ' + FULL_NAME + ' with HNGi7 ID ' + HNG_ID + ' and email ' + EMAIL + ' using ' + LANGUAGE + ' for stage 2 task'
-
 print("Je suis dans le place")
