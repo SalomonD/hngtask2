dee9428824a2f59f15d2b92d202390d0056c8df6
SalomonD <salomonnssow10@gmail.com>
2020-06-03 16:37:05 +0200

    change 2

--- scripts/donaldnsso.js
+++ scripts/donaldnsso.js
@@ -1,2 +1,1 @@
-console.log("Hello World, this is Salomon Donald NSSO BIDJA"+
-" with HNGi7 ID HNG-00906 and email salomonnssow10@gmail.com using Javascript for stage 2 task")
+console.log("Hello World, this is Salomon Donald NSSO BIDJA with HNGi7 ID HNG-00906 and email salomonnssow10@gmail.com using Javascript for stage 2 task")
